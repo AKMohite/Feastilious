142cd19b5252001fd47fb8784b0d79984bc3a9ed
Ashish Mohite <ashishmohite1996@gmail.com>
2022-10-08 20:49:29 +0530

    added version updates plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+apply(plugin = "com.github.ben-manes.versions")
 buildscript {
     repositories {
         google()
         jcenter()
+        gradlePluginPortal()
     }
     dependencies {
         classpath(libs.android.gradle.plugin)
         classpath(libs.kotlin.plugin)
         classpath(libs.hilt.plugin)
         classpath(libs.navigation.safe.args.plugin)
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -20,6 +23,29 @@ allprojects {
         google()
         jcenter()
     }
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    checkForGradleUpdate = true
+    outputFormatter = "html"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "dependency-updates-report"
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
 
 task<Delete>("clean") {
