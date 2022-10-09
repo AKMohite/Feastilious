6d06ccaf168989d2c06edc0fa6dea3362a19425a
Ashish Mohite <ashishmohite1996@gmail.com>
2022-10-09 12:11:00 +0530

    update dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,6 @@ dependencies {
     implementation(libs.coroutines.core)
     implementation(libs.data.store)
     implementation(libs.hilt.android)
-    implementation(libs.kotlin.stdlib)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.material)
