78c56f1447c9cfd7b0b6bd2b250d36fd18d60d83
Ashish Mohite <ashishmohite1996@gmail.com>
2024-12-31 11:23:09 +0530

    Add recyclerview for overview

--- app/src/main/java/com/ak/feastit/ui/yumdetail/components/overview/RecipeOverviewFragment.kt
+++ app/src/main/java/com/ak/feastit/ui/yumdetail/components/overview/RecipeOverviewFragment.kt
@@ -8,10 +8,12 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewbinding.ViewBinding
 import com.ak.feastit.base.BaseFragment
 import com.ak.feastit.databinding.FragmentDetailTabOverviewBinding
 import com.ak.feastit.ui.yumdetail.YumDetailViewModel
+import com.ak.feastit.ui.yumdetail.components.overview.component.RecipeOverviewItem
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -25,13 +27,24 @@ internal class RecipeOverviewFragment : BaseFragment() {
 
     private val viewModel: YumDetailViewModel by viewModels({ requireParentFragment() })
 
+    private val adapter: RecipeOverViewAdapter by lazy { RecipeOverViewAdapter() }
+
 
     override fun onViewReady(view: View, savedInstanceState: Bundle?) {
+        binding.overviewList.layoutManager = LinearLayoutManager(requireContext())
+        binding.overviewList.adapter = adapter
+
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.state.collectLatest { state ->
                     val overview = state.overview ?: return@collectLatest
-                    binding.recipeSummary.text = HtmlCompat.fromHtml(overview.recipeSummary, HtmlCompat.FROM_HTML_MODE_COMPACT)
+                    val items = listOf(
+                        RecipeOverviewItem.Heading("Summary"),
+                        RecipeOverviewItem.Text(overview.recipeSummary),
+                        RecipeOverviewItem.Heading("Similar Recipes"),
+                        RecipeOverviewItem.Text("all recipes in list")
+                    )
+                    adapter.reload(items)
                 }
             }
         }
