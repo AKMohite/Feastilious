4ef2a455a9aecc095d31066a9fb34aa708b15988
Ashish Mohite <ashishmohite1996@gmail.com>
2025-01-01 18:08:33 +0530

    WIP window insets edge to edge

--- app/src/main/java/com/ak/feastit/ui/MainActivity.kt
+++ app/src/main/java/com/ak/feastit/ui/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.ak.feastit.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.ak.feastit.R
 import com.ak.feastit.databinding.ActivityMainBinding
+import com.ak.feastit.utils.doOnApplyWindowInsets
 import com.ak.feastit.utils.hide
 import com.ak.feastit.utils.show
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,8 +26,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Default behavior, but we override the dark mode detection
+        // because we enable a user to change a theme in the settings
+        // and it has to be taken into the account here
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT,
+//                detectDarkMode = { true },
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                // The default light scrim, as defined by androidx and the platform.
+                // Taken from the EdgeToEdge.kt file.
+                lightScrim = Color.argb(0xe6, 0xFF, 0xFF, 0xFF),
+                // The default dark scrim, as defined by androidx and the platform.
+                // Taken from the EdgeToEdge.kt file.
+                darkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b),
+//                detectDarkMode = { true },
+            ),
+        )
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        setupView()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController= navHostFragment.navController
@@ -33,6 +61,15 @@ class MainActivity : AppCompatActivity() {
 
         binding.mainBottomNavigation.setupWithNavController(navController)
 
+    }
+
+    private fun setupView() {
+        binding.root.doOnApplyWindowInsets { _, insets, _, margins ->
+            val inset = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
+            binding.root.updateLayoutParams<MarginLayoutParams> {
+                bottomMargin = margins.bottom + inset
+            }
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/ak/feastit/utils/ViewExt.kt
+++ app/src/main/java/com/ak/feastit/utils/ViewExt.kt
@@ -3,7 +3,10 @@ package com.ak.feastit.utils
 import android.os.SystemClock
 import android.text.InputType
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 internal fun View.show(toShow: Boolean, gone: Boolean = true) {
     if (toShow) {
@@ -57,3 +60,63 @@ internal fun SearchView.setReadOnly(focusable: Boolean = true, inputType: Int = 
     isFocusableInTouchMode = !focusable
     this.inputType = inputType
 }
+
+/**
+ * https://chris.banes.dev/2019/04/12/insets-listeners-to-layouts/
+ */
+internal fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        requestApplyInsets()
+    } else {
+        addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.removeOnAttachStateChangeListener(this)
+                v.requestApplyInsets()
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
+}
+
+/**
+ * https://chris.banes.dev/2019/04/12/insets-listeners-to-layouts/
+ */
+internal fun View.doOnApplyWindowInsets(f: (View, WindowInsetsCompat, InitialSpacing, InitialSpacing) -> Unit) {
+    // Create a snapshot of the view's padding state
+    val initialPadding = recordInitialPaddingForView(this)
+    val initialMargin = recordInitialMarginForView(this)
+    // Set an actual OnApplyWindowInsetsListener which proxies to the given
+    // lambda, also passing in the original padding state
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        f(v, insets, initialPadding, initialMargin)
+        insets
+    }
+    // request some insets
+    requestApplyInsetsWhenAttached()
+}
+
+private fun recordInitialPaddingForView(view: View) =
+    InitialSpacing(
+        view.paddingLeft,
+        view.paddingTop,
+        view.paddingRight,
+        view.paddingBottom,
+    )
+
+private fun recordInitialMarginForView(view: View): InitialSpacing {
+    val lp = view.layoutParams as? ViewGroup.MarginLayoutParams
+    return InitialSpacing(
+        lp?.leftMargin ?: 0,
+        lp?.topMargin ?: 0,
+        lp?.rightMargin ?: 0,
+        lp?.bottomMargin ?: 0,
+    )
+}
+
+internal data class InitialSpacing(
+    val left: Int,
+    val top: Int,
+    val right: Int,
+    val bottom: Int,
+)
