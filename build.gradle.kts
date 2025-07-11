0d8254cc371d423e2864d1509039ce1162bbab75
Ashish Mohite <ashishmohite1996@gmail.com>
2025-07-11 22:27:18 +0530

    Add git hooks

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,25 @@ buildscript {
     }
 }
 
+tasks.register("copyGitHooks", Copy::class.java) {
+  description = "Copies the git hooks from /git-hooks to the .git folder."
+  group = "git hooks"
+  from("$rootDir/scripts/pre-commit")
+  into("$rootDir/.git/hooks/")
+}
+
+tasks.register("installGitHooks", Exec::class.java) {
+  description = "Installs the pre-commit git hooks from /git-hooks."
+  group = "git hooks"
+  workingDir = rootDir
+  commandLine = listOf("chmod")
+  args("-R", "+x", ".git/hooks/")
+  dependsOn("copyGitHooks")
+  doLast {
+    logger.info("Git hook installed successfully.")
+  }
+}
+
 subprojects {
     apply(plugin = "com.diffplug.spotless")
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
