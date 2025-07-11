131fbb01b5765d16d7ba0bf75b51c1735eb0ad5d
Ashish Mohite <ashishmohite1996@gmail.com>
2025-07-11 21:28:38 +0530

    Add spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,28 @@ buildscript {
         classpath(libs.navigation.safe.args.plugin)
         classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.1.0-1.0.29")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0")
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:7.1.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+    }
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("$buildDir/**/*.kt")
+
+            ktlint()
+            licenseHeaderFile(rootProject.file("spotless/feast-copyright.txt"))
+        }
+
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint()
+        }
     }
 }
 
