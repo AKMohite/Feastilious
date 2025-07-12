6de4b5258b50a1c1d465fcc517b3a0f1a24fff96
Ashish Mohite <ashishmohite1996@gmail.com>
2025-07-12 11:03:04 +0530

    Add favorite recipe widget. Need to handle preview layout and load recipes

---
+++ app/src/main/java/com/ak/feastit/ui/widgets/favoriterecipes/FavoriteRecipesWidgetProvider.kt
@@ -0,0 +1,18 @@
+// Copyright 2025, Ashish Mohite and the Yum Byte project contributors
+// License Name: <Actual name>
+package com.ak.feastit.ui.widgets.favoriterecipes
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+internal class FavoriteRecipesWidgetProvider : AppWidgetProvider() {
+
+  override fun onUpdate(
+    context: Context?,
+    appWidgetManager: AppWidgetManager?,
+    appWidgetIds: IntArray?,
+  ) {
+    super.onUpdate(context, appWidgetManager, appWidgetIds)
+  }
+}
