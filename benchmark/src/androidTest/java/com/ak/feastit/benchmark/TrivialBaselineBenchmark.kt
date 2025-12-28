6f2360d4dabd171608385d1991d3073018592815
Ashish Mohite <ashishmohite1996@gmail.com>
2025-12-28 16:15:57 +0530

    Fetch baseline profile and add to build and add tests for startupa and baseline to create profile

---
+++ benchmark/src/androidTest/java/com/ak/feastit/benchmark/TrivialBaselineBenchmark.kt
@@ -0,0 +1,39 @@
+package com.ak.feastit.benchmark
+
+import android.content.ComponentName
+import android.content.Intent
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TrivialBaselineBenchmark {
+
+  @get:Rule
+  val rule = BaselineProfileRule()
+
+  @Test
+  fun trivialStartup() {
+    trackStartup()
+  }
+
+  private fun trackStartup() {
+    rule.collect(
+      packageName = APP_TO_BENCHMARK
+    ) {
+      val intent = Intent()
+      intent.component = ComponentName(
+        /* pkg = */ APP_TO_BENCHMARK,
+        /* cls = */ "com.ak.feastit.ui.main.MainActivity"
+      )
+      startActivityAndWait(intent)
+    }
+  }
+
+  companion object {
+    const val APP_TO_BENCHMARK = "com.ak.feastit"
+  }
+
+}
